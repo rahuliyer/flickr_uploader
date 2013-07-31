28005ce669684aef0064e44a91985aaca661a208
Rahul Iyer <riyer@riyermbp15.local>
2013-07-30 22:10:32 -0700

    Authentication pieces for the flickr api

---
+++ auth.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+import os
+import sys
+import json
+import os.path as path
+import urllib2
+import urllib
+
+import oauth
+
+AUTH_TOKEN_FILE = "~/.flickr_token"
+REQUEST_TOKEN_URL = "http://www.flickr.com/services/oauth/request_token"
+AUTHORIZE_URL = "http://www.flickr.com/services/oauth/authorize"
+ACCESS_TOKEN_URL = "http://www.flickr.com/services/oauth/access_token"
+
+class Auth:
+	def __init__(self, app_id, app_secret):
+		self.appId = app_id
+		self.appSecret = app_secret
+		self.oauth = oauth.OAuth(app_id, app_secret, REQUEST_TOKEN_URL, ACCESS_TOKEN_URL, self.authorizeCb)
+
+	def authorizeCb(self, request_token):
+		url = AUTHORIZE_URL + "?oauth_token=" + request_token
+		print "Please go to " + url + " to authorize the app."
+		return raw_input("Enter the verifier on the page: ")
+
+	def authenticate(self):
+		creds = None
+		try:
+			creds = self.readAuthToken()
+		except:
+			pass
+
+		if creds != None:
+			self.oauth.setCredentials(creds["token"], creds["secret"])
+		else:
+			self.oauth.authenticate()
+			self.writeToken(self.oauth.getAccessToken(), self.oauth.getAccessTokenSecret())
+		print self.oauth.getAccessToken()
+		print self.oauth.getAccessTokenSecret()
+
+	def getAppId(self):
+		return self.appId
+
+	def createGetRequest(self, url, params):
+		return urllib2.Request(url + "?" + urllib.urlencode(params))
+		
+	def createPostRequest(self, url, params):
+		return urllib2.Request(url, urllib.urlencode(params))
+		
+	def createOauthGetRequest(self, url, params):
+		return self.oauth.createOauthRequest("GET", url, params, self.oauth.getAccessToken(), self.oauth.getAccessTokenSecret())
+			
+	def createOauthPostRequest(self, url, params):
+		return self.oauth.createOauthRequest("POST", url, params, self.oauth.getAccessToken(), self.oauth.getAccessTokenSecret())
+			
+	def writeToken(self, token, token_secret):
+		d = { "token": token, "secret": token_secret }
+		serialized_str = json.dumps(d)
+
+		try:
+			fd = os.open(path.expanduser(AUTH_TOKEN_FILE), 
+				os.O_CREAT | os.O_WRONLY | os.O_TRUNC, 0600)
+			handle = os.fdopen(fd, "w")
+			handle.write(serialized_str)
+			handle.close()
+
+		except:
+			sys.stderr.write("Error writing token to file!\n")
+			raise
+
+	def readAuthToken(self):
+		file_path = path.expanduser(AUTH_TOKEN_FILE)
+		if not path.exists(file_path):
+			return None
+
+		try:
+			handle = open(file_path, "r")
+			data = handle.read()
+			return json.loads(data)
+		except:
+			sys.stderr.write("Error reading token from token file")
+			raise
+
---
+++ oauth.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+
+import os
+import sys
+import json
+import os.path as path
+import time
+import hashlib
+import hmac
+import base64
+import urllib
+import urllib2
+import urlparse
+import random
+
+class OAuth:
+	def __init__(self, app_id, app_secret, req_token_url, access_token_url, authorize_cb):
+		self.appId = app_id
+		self.appSecret = app_secret
+		self.requestTokenUrl = req_token_url
+		self.accessTokenUrl = access_token_url
+		self.authorizeCb = authorize_cb
+
+		self.requestToken = ""
+		self.requestTokenSecret = ""
+		self.accessToken = ""
+		self.accessTokenSecret = ""
+		self.authVerifier = ""
+	
+	def setCredentials(self, access_token, token_secret):
+		self.accessToken = access_token
+		self.accessTokenSecret = token_secret
+
+	def authenticate(self):
+		self.fetchRequestToken()
+		self.authorize()
+		self.fetchAccessToken()
+
+	def fetchRequestToken(self):
+		url = self.requestTokenUrl
+		params = {"oauth_callback": "oob"}
+		request = self.createOauthRequest("GET", url, params, "", "")
+		handler = urllib2.urlopen(request)
+		res = urlparse.parse_qs(handler.read())
+		self.requestToken = res["oauth_token"][0]
+		self.requestTokenSecret = res["oauth_token_secret"][0]
+
+	def authorize(self):
+		self.authVerifier = self.authorizeCb(self.requestToken)
+
+	def fetchAccessToken(self):
+		url = self.accessTokenUrl
+		params = {"oauth_verifier": self.authVerifier}
+		request = self.createOauthRequest("GET", url, params, self.requestToken, self.requestTokenSecret)
+		handler = urllib2.urlopen(request)
+		res = urlparse.parse_qs(handler.read())
+		self.accessToken = res["oauth_token"][0]
+		self.accessTokenSecret = res["oauth_token_secret"][0]
+
+	def getAccessToken(self):
+		return self.accessToken
+
+	def getAccessTokenSecret(self):
+		return self.accessTokenSecret
+
+	def createOauthRequest(self, method, url, params, token="", token_secret=""):
+		self.addOauthParams(method, url, params, token, token_secret)
+		if method == "GET":
+			return urllib2.Request(url + "?" + urllib.urlencode(params))
+		else:
+			return urllib2.Request(url, urllib.urlencode(params))
+			
+
+	def addOauthParams(self, method, url, params, token, token_secret):
+		ts = int(time.time())
+		nonce = str(ts) + str(random.randint(0, sys.maxint))
+
+		if token != "":
+			params["oauth_token"] = token
+
+		params["oauth_nonce"] = nonce
+		params["oauth_timestamp"] = ts
+		params["oauth_consumer_key"] = self.appId
+		params["oauth_version"] = "1.0"
+		params["oauth_signature_method"] = "HMAC-SHA1"
+		params["oauth_signature"] = self.getRequestSignature(method, url, params, token_secret)
+
+	def getRequestSignature(self, method, url, params, token_secret = ""):
+		base_str = method + "&" + urllib.quote_plus(url) + "&"
+
+		param_str = ""
+		for i in sorted(params.keys()):
+			param_str += i + "=" + str(params[i]).replace(" ", "+") + "&"
+
+		param_str = urllib.quote_plus(param_str[:-1])
+		base_str += param_str
+
+		key = str(self.appSecret + "&" + token_secret)
+		sig = hmac.new(key, base_str, hashlib.sha1).digest()
+
+		return base64.b64encode(sig)
