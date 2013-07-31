7a880dc93cbf3f831016c06c67909a741aae9167
Rahul Iyer <riyer@riyermbp15.local>
2013-07-31 02:46:41 -0700

    Add a generic wrapper for flickr api

---
+++ flickrapi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import auth
+
+class FlickrApi:
+	def __init__(self, auth):
+		self.auth = auth
+
+	def addApiArgs(self, method, args):
+		args["method"] = method
+		args["format"] = "json"
+		args["nojsoncallback"] = 1
+		args["api_key"] = self.auth.getAppId()
+
+	def createRestGetRequest(self, method, args):
+		self.addApiArgs(method, args)
+		return self.auth.createGetRequest("http://api.flickr.com/services/rest", args)
+
+	def createRestPostRequest(self, method, args):
+		self.addApiArgs(method, args)
+		return self.auth.createPostRequest("http://api.flickr.com/services/rest", args)
+
+	def createAuthenticatedRestGetRequest(self, method, args):
+		self.addApiArgs(method, args)
+		return self.auth.createOauthGetRequest("http://api.flickr.com/services/rest", args)
+
+	def createAuthenticatedRestPostRequest(self, method, args):
+		self.addApiArgs(method, args)
+		return self.auth.createOauthPostRequest("http://api.flickr.com/services/rest", args)
